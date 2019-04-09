91db9fd50d40448402be5c2ae5f486a6a09f2f1f
Jeframos <jefferson.ramos10@gmail.com>
2019-04-09 07:56:37 -0300

    Implementação de código

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const readline = require('readline-sync')
+
+
 function start(){
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
     function askAndReturnSearchTerm(){
         return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+
+        console.log(selectPrefixIndex)
     }
 
     console.log(content)
